937bf67a50e1a40bc93936678fce5990ff7869f4
gitfilix <runlevel07@gmail.com>
2021-06-07 15:51:29 +0200

    storing photos in a array and display them

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -12,18 +12,36 @@ import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Storage } from "@capacitor/storage";
 import { Capacitor } from "@capacitor/core";
 
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
 
 export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
   // take a picture
   const takePhoto = async () => {
+    const fileName = new Date().getTime() + ".jpeg";
     const cameraPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 90,
     })
+    
+    const newPhotos = [
+        {
+          filepath: fileName,
+          webviewPath: cameraPhoto.webPath,
+        },
+        ...photos,
+      ];
+    setPhotos(newPhotos);
   }
 
   return {
+    photos,
     takePhoto,
   }
 }
