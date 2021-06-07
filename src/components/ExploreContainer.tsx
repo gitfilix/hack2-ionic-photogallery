ae4fcc03247f28e17388d782c9b61f08a00fadb7
gitfilix <runlevel07@gmail.com>
2021-06-07 18:26:50 +0200

    just to have a ugly image on statrt.

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -8,7 +8,8 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components!</a></p>
+      <img src='/assets/images/relightMyFire.jpg' alt='relight'  width='400px' />
     </div>
   );
 };
